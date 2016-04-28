ead368441748712aae9f97dbb6aedc7f7abc38b0
Jamal Eddine Ka <J3K@localhost.localdomain>
2016-04-28 13:41:10 +0100

    Minified JS. edit on Modal titles

--- oldScripts/ngLHController.js
+++ oldScripts/ngLHController.js
@@ -1,6 +1,5 @@
 angular
-.module('ngLiveHighlights')
-.controller('ngLHController', function ($scope, $sce, ngLHFactory, DTOptionsBuilder, DTColumnBuilder, DTColumnDefBuilder)
+.module('ngLiveHighlights').controller('HighlightsAndGoals', function ($scope, $rootScope,$sce, AdsEngine, DTOptionsBuilder, DTColumnBuilder, DTColumnDefBuilder)
 {
     $scope.Title = " LiveHighlights ";
     $scope.FootballGames;
@@ -23,28 +22,7 @@ angular
     $scope.ShowAdLinkSource = true;
     $scope.date = new Date().getTime();
 
-    ngLHFactory.getAds().success(function (getAds)
-    {
-        $scope.FootballGames = getAds;
-    }
-    ).error(function (error)
-    {
-        console.log(error);
-    }
-    );
-
-
-    ngLHFactory.getSecondAdsArray().success(function (getSecondAdsArray)
-    {
-        $scope.AllSports = getSecondAdsArray;
-    }
-    ).error(function (error)
-    {
-        console.log(error);
-    }
-    );
-
-    ngLHFactory.getFootballHighlightsv1().success(function (FootballHighlightsv1)
+    AdsEngine.getFootballHighlightsv1().success(function (FootballHighlightsv1)
     {
         $scope.FootballHighlightsv1 = FootballHighlightsv1;
         // console.log(FootballHighlightsv1.length);
@@ -55,7 +33,7 @@ angular
     }
     );
 
-    ngLHFactory.getFootballHighlightsv2().success(function (FootballHighlightsv2)
+    AdsEngine.getFootballHighlightsv2().success(function (FootballHighlightsv2)
     {
         $scope.FootballHighlightsv2 = FootballHighlightsv2;
         // console.log("LENGTH" + FootballHighlightsv2.length);
@@ -66,66 +44,13 @@ angular
     }
     );
 
-    $scope.WatchGame = function (argument, VideoTitle)
-    {
-
-        // console.log(argument);
-
-        // $('#LiveGamesHighlights').openModal();
-        $scope.TITLE = VideoTitle;
-
-        $('#LiveGames').openModal(
-        {
-            dismissible : true, // Modal can be dismissed by clicking outside of the modal
-            opacity : .5, // Opacity of modal background
-            in_duration : 300, // Transition in duration
-            out_duration : 200, // Transition out duration
-            ready : function ()
-            {
-                $('ul.tabs').tabs('select_tab', 'Link7');
-            }, // Callback for Modal open
-            complete : function ()
-            {
-                $("#LiveGames").find('iframe').attr('src', "");
-            } // Callback for Modal close
-        }
-        );
-        console.log($scope.ShowAdLinkSource);
-        console.log($scope.ShowAdLinkSource);
-
-        if (!$scope.PlayerExpanded)
-        {
-            $scope.PlayerExpanded = !$scope.PlayerExpanded;
-            $scope.isHighlights = false;
-        }
-        else
-            $scope.PlayerExpanded = true;
-
-        $scope.Links = argument;
-    }
-
-    $scope.ClickedLink = function (key)
-    {
-        // console.log(key);
-
-        // console.log($scope.PlayerExpanded);
-        // if(!$scope.PlayerExpanded)
-        // {
-        //     $scope.PlayerExpanded = !$scope.PlayerExpanded;
-        //     $scope.isHighlights = false;
-        // }
-        // else $scope.PlayerExpanded = true;
-        $scope.PlayerCode = $sce.trustAsResourceUrl(key);
-        // console.log($scope.PlayerExpanded);
-
-    }
 
     $scope.ClickedHighlightsLink = function (key, VideoTitle)
     {
 
         // console.log(key);
 
-        $scope.TITLE = VideoTitle;
+        $scope.$parent.TITLE = VideoTitle;
 
         $('#Highlights').openModal(
         {
@@ -144,126 +69,25 @@ angular
         }
         );
 
-        // console.log($scope.PlayerExpanded);
-        if (!$scope.PlayerExpanded)
-        {
-            $scope.PlayerExpanded = !$scope.PlayerExpanded;
-            $scope.isHighlights = true;
-        }
-        else
-            $scope.PlayerExpanded = true;
+        // // console.log($scope.PlayerExpanded);
+        // if (!$scope.$parent.PlayerExpanded)
+        // {
+        //     $scope.$parent.PlayerExpanded = !$scope.PlayerExpanded;
+        //     $scope.$parent.isHighlights = true;
+        // }
+        // else
+        //     $scope.$parent.PlayerExpanded = true;
 
-        $scope.HighlightsCode = $sce.trustAsResourceUrl("http://cdn.livehighlights.net/content/web" + key);
-        // console.log($scope.PlayerExpanded);
-
-    }
-
-
-
-
-    $scope.ClickedSportLink = function (key, GameA,GameB,WEB,P2P)
-    {
-
-        // console.log(key);
-
-        $scope.TITLE = GameA + " - " + GameB;
-
-        $scope.TableP2P = $sce.trustAsHtml(P2P);
-        $scope.TableWEB = $sce.trustAsHtml(WEB);
-                
-        $('#Sports').openModal(
-        {
-            dismissible : true, // Modal can be dismissed by clicking outside of the modal
-            opacity : .5, // Opacity of modal background
-            in_duration : 300, // Transition in duration
-            out_duration : 200, // Transition out duration
-            ready : function ()
-            {
-                // $('ul.tabs').tabs('select_tab', '#Link0');
-                // console.log(WEB);
-                // console.log(P2P);
-
-                $("#StreamTable th:first-child").remove();
-                $("#StreamTable td:first-child").remove();
-
-            }, // Callback for Modal open
-            complete : function ()
-            {
-                $("#Sports").find('iframe').attr('src', "");
-            } // Callback for Modal close
-        }
-        );
-
-        // console.log($scope.PlayerExpanded);
-        if (!$scope.PlayerExpanded)
-        {
-            $scope.PlayerExpanded = !$scope.PlayerExpanded;
-        }
-        else
-            $scope.PlayerExpanded = true;
-
+        $scope.$parent.HighlightsCode = $sce.trustAsResourceUrl("http://cdn.livehighlights.net/content/web" + key);
         // console.log($scope.PlayerExpanded);
 
     }
-
-    var vm = this;
-
-    // console.log($scope.FootballGames);
-    if (!$scope.FootballGames)
-    {
-        vm.dtOptions0 = DTOptionsBuilder.newOptions()
-            .withDOM("<'row'<'col s6'p><'col s6'f>><'row'<'col s12't>><'row'<'col s12'p>>") // pitrfl
-            .withOption('language',
-            {
-                "emptyTable" : "<div class=\"card-panel teal lighten-2\"><div class=\"center-align\"> No game broadcasted right now </div></div>",
-                "searchPlaceholder" : " League,Time,Teams and Language"
-            }
-            )
-            .withOption('order', [1, 'asc'])
-            .withOption('responsive', true)
-            .withOption('scroller',
-            {
-                'loadingIndicator' : true
-            }
-            )
-            // .withOption('aaSorting', [ 2, 'asc' ])
-            // .withOption('stateSave', true)
-            .withOption('pageLength', 15);
-    }
-    else
-    {
-
-        vm.dtOptions0 = DTOptionsBuilder.newOptions()
-            .withDOM("<'row'<'col s6'p><'col s6'f>><'row'<'col s12't>><'row'<'col s12'p>>") // pitrfl
-            .withOption('language',
-            {
-                "emptyTable" : "<div class=\"progress\"><div class=\"indeterminate\"></div></div>",
-                "searchPlaceholder" : " League,Time,Teams and Language"
-            }
-            )
-            .withOption('order', [1, 'asc'])
-            .withOption('responsive', true)
-            .withOption('scroller',
-            {
-                'loadingIndicator' : true
-            }
-            )
-            // .withOption('aaSorting', [ 2, 'asc' ])
-            // .withOption('stateSave', true)
-            .withOption('pageLength', 15);
-
-    }
-    vm.dtColumnDefs0 = [
-        DTColumnDefBuilder.newColumnDef(0).withOption('width', '25%').notSortable(),
-        DTColumnDefBuilder.newColumnDef(1).withOption('width', '5%').notSortable(),
-        DTColumnDefBuilder.newColumnDef(2).withOption('width', '40%').notSortable(),
-        DTColumnDefBuilder.newColumnDef(3).withOption('width', '10%').notSortable(),
-        DTColumnDefBuilder.newColumnDef(4).withOption('width', '20%').notSortable(),
-        // DTColumnDefBuilder.newColumnDef(5).withOption('width', '35%').notSortable()
-    ];
-
+    
+    
+    var vm = $rootScope;
+        
     vm.dtOptions1 = DTOptionsBuilder.newOptions()
-        .withDOM("<'row'<'col s6'p><'col s6'f>><'row'<'col s12't>><'row'<'col s12'p>>") // pitrfl
+        .withDOM("<'row'<'col s12 m6'p><'col s12 m6'f>><'row'<'col s12't>><'row'<'col s12'p>>") // pitrfl
         .withOption('language',{ "emptyTable" : "<div class=\"progress\"><div class=\"indeterminate\"></div></div>",
             "searchPlaceholder" : " Teams and Scores"
         })
@@ -283,7 +107,7 @@ angular
     ];
 
     vm.dtOptions2 = DTOptionsBuilder.newOptions()
-        .withDOM("<'row'<'col s6'p><'col s6'f>><'row'<'col s12't>><'row'<'col s12'p>>") // pitrfl
+        .withDOM("<'row'<'col s12 m6'p><'col s12 m6'f>><'row'<'col s12't>><'row'<'col s12'p>>") // pitrfl
         .withOption('language',
         {
             "emptyTable" : "<div class=\"progress\"><div class=\"indeterminate\"></div></div>",
@@ -301,10 +125,208 @@ angular
     ];
 
 
+}).filter('split', function (){
+    return function (input, splitChar, splitIndex)
+    {
+        // do some bounds checking here to ensure it has that index
+        if(input) { return input.split(splitChar)[splitIndex]; }
+    }
+}).filter('splitOnly', function (){
+    return function (input, splitChar)
+    {
+        // do some bounds checking here to ensure it has that index
+        if(input) { return input.split(splitChar); }
+    }
+});
+
+
+angular.module('ngLiveHighlights').controller('LiveFootballScontroller', function($scope, $rootScope, $sce, AdsEngine, DTOptionsBuilder, DTColumnBuilder, DTColumnDefBuilder)
+{
+    
+    $rootScope.$on('$routeChangeStart', function(event, currRoute, prevRoute){
+		$rootScope.animation = currRoute.animation;
+        // alert("asd  ");
+	  });
+      
+    $scope.Title = " LiveHighlights ";
+    $scope.FootballGames;
+    $scope.AllSports;
+
+    $scope.sortType = 'Time'; // set the default sort type
+    $scope.sortType2 = 'TeamAvsTeamB'; // set the default sort type
+
+
+    $scope.sortReverse = false; // set the default sort order
+    $scope.searchGame = ''; // set the default search/filter term
+    $scope.searchGamev2 = ''; // set the default search/filter term
+    $scope.searchGamev3 = ''; // set the default search/filter term
+    $scope.ShowAdLinkSource = true;
+    $scope.date = new Date().getTime();
+
+    AdsEngine.getAds().success(function (getAds)
+    {
+        $scope.FootballGames = getAds;
+    }).error(function (error)
+    {
+        console.log(error);
+    });
+
+    AdsEngine.getSecondAdsArray().success(function (getSecondAdsArray)
+    {
+        $scope.AllSports = getSecondAdsArray;
+    }).error(function (error)
+    {
+        console.log(error);
+    });
+    
+    $scope.WatchGame = function (argument, VideoTitle)
+    {
+        $scope.$parent.TITLE = VideoTitle;
+
+        $('#LiveGames').openModal(
+        {
+            dismissible : true, // Modal can be dismissed by clicking outside of the modal
+            opacity : .5, // Opacity of modal background
+            in_duration : 300, // Transition in duration
+            out_duration : 200, // Transition out duration
+            ready : function ()
+            {
+                $('ul.tabs').tabs('select_tab', 'Link7');
+                
+            }, // Callback for Modal open
+            complete : function ()
+            {
+                $("#LiveGames").find('iframe').attr('src', "");
+            } // Callback for Modal close
+        }
+        );
+        
+
+        $scope.$parent.Links = argument;
+    }
+
+    $scope.$parent.ClickedLink = function (key)
+    {
+        //    alert("asdasdas");
+        //    alert(val(key+1));
+        // $('Link' + val(key+1)).css("display", "block");
+        $scope.$parent.PlayerCode = $sce.trustAsResourceUrl(key);
+    }
+    
+    var i=0;
+
+    $scope.$parent.ClickedSportLink = function (key, GameA,GameB,WEB,P2P)
+    {   
+        
+        // console.log(key);
+
+        $scope.$parent.TITLE = GameA + " - " + GameB;
+
+        $scope.$parent.TableP2P = $sce.trustAsHtml(P2P);
+        $scope.$parent.TableWEB = $sce.trustAsHtml(WEB);
+                
+                
+        $('#Sports').openModal(
+        {
+            dismissible : true, // Modal can be dismissed by clicking outside of the modal
+            opacity : .5, // Opacity of modal background
+            in_duration : 300, // Transition in duration
+            out_duration : 200, // Transition out duration
+            ready : function ()
+            {
+                // if(i==0)
+                // {
+                    // alert(i);
+                    $("#StreamTable span.votestreams").remove();
+                    $("#StreamTable th:first-child").remove();
+                    $("#StreamTable thead tr").prepend('<th>');
+                    // $("#StreamTable td:first-child").remove();
+                    // i++;
+                    // alert( " i == 0 ");
+                // } else alert( " i != 0 ");
+                // } 
+                // $('ul.tabs').tabs('select_tab', '#Link0');
+                // console.log(WEB);
+                // console.log(P2P);
+            }, // Callback for Modal open
+            complete : function ()
+            {
+                $("#Sports").find('iframe').attr('src', "");
+                // i=0;
+            } // Callback for Modal close
+        });
+
+        // console.log($scope.PlayerExpanded);
+        if (!$scope.PlayerExpanded)
+        {
+            $scope.$parent.PlayerExpanded = !$scope.PlayerExpanded;
+        }
+        else
+            $scope.$parent.PlayerExpanded = true;
+
+        // console.log($scope.PlayerExpanded);
+        // i = 0;
+    }
+
+    i=0;
+    
+    
+    var vm = $rootScope;
+
+    // console.log($scope.FootballGames);
+    if (!$scope.$parent.FootballGames)
+    {
+        vm.dtOptions0 = DTOptionsBuilder.newOptions()
+            .withDOM("<'row'<'col s12 m6'p><'col s12 m6'f>><'row'<'col s12't>><'row'<'col s12'p>>") // pitrfl
+            .withOption('language',
+            {
+                "emptyTable" : "<div class=\"card-panel teal lighten-2\"><div class=\"center-align\"> No game broadcasted right now </div></div>",
+                "searchPlaceholder" : " League,Time,Teams and Language"
+            })
+            .withOption('order', [1, 'asc'])
+            .withOption('responsive', true)
+            // .withOption('responsive', { details: { type: 'column', target: [0,2,5] }})
+            .withOption('scroller',
+            {
+                'loadingIndicator' : true
+            })
+            // .withOption('aaSorting', [ 2, 'asc' ])
+            // .withOption('stateSave', true)
+            .withOption('pageLength', 15);
+    }
+    else
+    {
+
+        vm.dtOptions0 = DTOptionsBuilder.newOptions()
+            .withDOM("<'row'<'col s12 m6'p><'col s12 m6'f>><'row'<'col s12't>><'row'<'col s12'p>>") // pitrfl
+            .withOption('language',
+            {
+                "emptyTable" : "<div class=\"progress\"><div class=\"indeterminate\"></div></div>",
+                "searchPlaceholder" : " League,Time,Teams and Language"
+            })
+            .withOption('order', [1, 'asc'])
+            .withOption('responsive', true)
+            .withOption('scroller',
+            {
+                'loadingIndicator' : true
+            })
+            // .withOption('aaSorting', [ 2, 'asc' ])
+            // .withOption('stateSave', true)
+            .withOption('pageLength', 15);
+    }
+    
+    vm.dtColumnDefs0 = [
+        DTColumnDefBuilder.newColumnDef(0).withOption('width', '25%').notSortable(),
+        DTColumnDefBuilder.newColumnDef(1).withOption('width', '5%').notSortable(),
+        DTColumnDefBuilder.newColumnDef(2).withOption('width', '40%').notSortable(),
+        DTColumnDefBuilder.newColumnDef(3).withOption('width', '10%').notSortable(),
+        DTColumnDefBuilder.newColumnDef(4).withOption('width', '20%').notSortable(),
+        // DTColumnDefBuilder.newColumnDef(5).withOption('width', '35%').notSortable()
+    ];
 
 
     vm.dtOptions3 = DTOptionsBuilder.newOptions()
-        .withDOM("<'row'<'col s6'p><'col s6'f>><'row'<'col s12't>><'row'<'col s12'p>>") // pitrfl
+        .withDOM("<'row'<'col s12 m6'p><'col s12 m6'f>><'row'<'col s12't>><'row'<'col s12'p>>") // pitrfl
         .withOption('language',{ "emptyTable" : "<div class=\"progress\"><div class=\"indeterminate\"></div></div>", "searchPlaceholder" : " League,Time,Teams and Language"})
         .withOption('responsive',true)
         // .withOption('order', [1, 'desc'])
@@ -315,29 +337,25 @@ angular
     vm.dtColumnDefs3 = [
         DTColumnDefBuilder.newColumnDef(0).withOption('width', '5%').notSortable(),
         DTColumnDefBuilder.newColumnDef(1).withOption('width', '5%').notSortable(),
-        DTColumnDefBuilder.newColumnDef(2).withOption('width', '30%').notSortable(),
-        DTColumnDefBuilder.newColumnDef(3).withOption('width', '30%').notSortable(),
-        DTColumnDefBuilder.newColumnDef(4).withOption('width', '20%').notSortable()
+        DTColumnDefBuilder.newColumnDef(2).withOption('width', '25%').notSortable(),
+        DTColumnDefBuilder.newColumnDef(3).withOption('width', '40%').notSortable(),
+        DTColumnDefBuilder.newColumnDef(4).withOption('width', '15%').notSortable()
         // DTColumnDefBuilder.newColumnDef(4).withOption('width', '20%').notSortable(),
         // DTColumnDefBuilder.newColumnDef(5).withOption('width', '10%').notSortable()
     ];
 
-}
-).filter('split', function ()
-{
+    
+}).filter('split', function (){
     return function (input, splitChar, splitIndex)
     {
         // do some bounds checking here to ensure it has that index
         if(input) { return input.split(splitChar)[splitIndex]; }
     }
-}
-).filter('splitOnly', function ()
-{
+}).filter('splitOnly', function (){
     return function (input, splitChar)
     {
 
         // do some bounds checking here to ensure it has that index
         if(input) { return input.split(splitChar); }
     }
-}
-);
+});
---
+++ test.js
@@ -0,0 +1,41 @@
+angular
+.module('ngLiveHighlights', ['datatables','angularMoment','ngSanitize','ngRoute','ngAnimate'])
+.config(["$routeProvider", function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl : 'pages/HomePage.html',
+                controller  : 'LiveFootballScontroller',
+                animation   : 'ShowView'
+            })
+
+            .when('/Goals', {
+                templateUrl : 'pages/HighlightsAndGoals.html',
+                controller  : 'HighlightsAndGoals',
+                animation   : 'ShowView'
+            })
+
+            .when('/About', {
+                templateUrl : 'pages/About.html',
+                controller  : 'HighlightsAndGoals',
+                animation   : 'ShowView'
+            })
+            .otherwise({
+                redirectTo:'/',
+                animation   : 'ShowView'
+            });
+    }]);
+    
+ $(document).ready(function(){
+     
+  $(".button-collapse").sideNav();
+  
+  // $('.RightMenuNav').sideNav('show');
+  // $('.slide-out').sideNav('show');
+  
+    $('ul.tabs').tabs();
+  });
+      
+      
+      
+      
+      
